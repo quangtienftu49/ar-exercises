a5eaabed86c70b3ac978753a84be9db8fc8f05a8
quangtienftu49 <quangtienftu49@gmail.com>
2022-02-01 00:19:57 -0500

    done exercise_7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
+class MenOrWomensApparel < ActiveModel::Validator
+  def validate(record)
+    unless record.mens_apparel || record.womens_apparel
+      record.errors[:name] << 'must carry either mens or womens apparel'
+    end
+  end
+end
+
+
 class Store < ActiveRecord::Base
   has_many :employees
+  validates_associated :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+  validates_with MenOrWomensApparel
 end
